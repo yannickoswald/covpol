f1083e63c1fa3efbf37f41882d20669bf46799c5
Yannick Oswald <52912030+eeyouol@users.noreply.github.com>
2023-01-03 14:18:45 +0000

    Delete older model versions directory

--- older model versions/data_calibrater.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 25 14:17:39 2022
-
-@author: earyo
-"""
-
-## compare model parameter configurations apply simple least squares method on mean and data as well as 
-## consider variance of model runs. lower variance better than higher variance
